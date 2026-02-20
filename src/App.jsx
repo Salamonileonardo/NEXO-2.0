0c8a544d9adc894e312278a9dfd251eee67210e1
Salamonileonardo <salamonileonardo5@gmail.com>
2026-02-20 19:47:48 -0300

    criação arquivos

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Avatar from "@mui/material/Avatar";
+
+function App() {
+  return (
+    <>
+      <div className="pagina-inteira">
+        <div className="pagina-celular">
+          <img className="logo" src={logo} alt="Logo" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
